3249987980fe07711f6dc7e74cef970986bc6e8b
Eurybus <wraitholyth@gmail.com>
2016-01-31 11:47:36 +0200

    PowerUp respawn implemented.

--- Old Gods of the Forest/Assets/scripts/Level/pwrUpBase.cs
+++ Old Gods of the Forest/Assets/scripts/Level/pwrUpBase.cs
@@ -4,19 +4,35 @@ using System.Collections;
 public class pwrUpBase : MonoBehaviour {
 
     bool pickedUp;
+    float respawnTimer = 20f;
+    Sprite defaultSprite;
 	// Use this for initialization
 	void Start () {
-	
+        defaultSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void FixedUpdate()
+    {
+        //Resspawn timer
 
+        if (pickedUp)
+        {
+
+            respawnTimer -= Time.deltaTime;
+            if (respawnTimer < 0)
+            {
+                pickedUp = false;
+                gameObject.GetComponent<SpriteRenderer>().sprite = defaultSprite;
+            }
+        }
+    }
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "player")
+        if (col.gameObject.name == "Hero")
         {
             //let's hide this object
             gameObject.GetComponent<SpriteRenderer>().sprite = null;
